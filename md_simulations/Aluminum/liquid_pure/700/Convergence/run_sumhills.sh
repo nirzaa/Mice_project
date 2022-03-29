ee6d455581d0a6b7b5190e32583afa8f86f01a7c
nirzaa <72485675+nirzaa@users.noreply.github.com>
2022-03-29 14:14:26 +0300

    updated

--- md_simulations/Aluminum/liquid_pure/700/Convergence/run_sumhills.sh
+++ md_simulations/Aluminum/liquid_pure/700/Convergence/run_sumhills.sh
@@ -1,1 +1,4 @@
-nohup plumed --no-mpi sum_hills --hills ../HILLS --stride 1000 --mintozero --bin 200,200 &
+export LD_LIBRARY_PATH=/home/nirz/lmp2021/lib/plumed/plumed-2.7.1/src/lib
+#cd /home/nirz/lmp2021/lib/plumed/plumed-2.7.1/
+#source sourceme.sh
+nohup /home/nirz/lmp2021/lib/plumed/plumed2/bin/plumed --no-mpi sum_hills --hills ../HILLS --stride 1000 --mintozero --bin 200,200 &
